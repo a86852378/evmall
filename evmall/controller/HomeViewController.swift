d08dcd26d3149fcdc4685ed8b820642f2df1a462
黄伟祥 <75646880@qq.com>
2016-09-20 17:51:56 +0800

    将collectionview嵌入tableview里

--- evmall/controller/HomeViewController.swift
+++ evmall/controller/HomeViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class HomeViewController: UITableViewController{
+class HomeViewController: UITableViewController,UICollectionViewDelegate,UICollectionViewDataSource{
 
 
     var pageControl = UIPageControl()
     var scrollView = UIScrollView()
-//    let pageControl = UIPageControl()
+    var collectionView = UICollectionView(frame: CGRectMake(0, 0, 0, 0), collectionViewLayout: UICollectionViewFlowLayout())
     var timer: NSTimer?
     var rollingTime: NSTimeInterval = 3.0
     
@@ -24,19 +24,9 @@ class HomeViewController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        
-        
-//        serviceController.getData(){(array) in
-//            self.array = array as! [Goodsclass]
-//            dispatch_async(dispatch_get_main_queue(),{self.collectionView.reloadData()})
-//        }
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
-//        collectionView.backgroundColor = UIColor.whiteColor()
-//        
-        
         tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        
+        
                 // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -46,55 +36,77 @@ class HomeViewController: UITableViewController{
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 150
+        var height: CGFloat = 0
+        if(indexPath.row == 0){
+            height = 120
+        }
+        else if(indexPath.row == 1){
+            height = 130
+        }
+        return height
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        
-        scrollView.pagingEnabled = true
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.scrollEnabled = true
-        scrollView.frame = CGRectMake(0, 0, 320, 150)
-        for i in 0...4{
-            var picName = "广告"
-            switch i {
-            case 0:
-                picName = picName + "3"
-            case 4:
-                picName = picName + "1"
-            default:
-                picName = picName + i.description
-            }
-            let imageView =  UIImageView(image: UIImage(named: picName))
+        //广告自动循环轮播
+        if(indexPath.row == 0){
+            scrollView.pagingEnabled = true
+            scrollView.showsHorizontalScrollIndicator = false
+            scrollView.showsVerticalScrollIndicator = false
+            scrollView.scrollEnabled = true
+            scrollView.frame = CGRectMake(0, 0, 320, 120)
+            for i in 0...4{
+                var picName = "广告"
+                switch i {
+                case 0:
+                    picName = picName + "3"
+                case 4:
+                    picName = picName + "1"
+                default:
+                    picName = picName + i.description
+                }
+                let imageView =  UIImageView(image: UIImage(named: picName))
             
-            imageView.frame = CGRectMake(scrollView.frame.width*CGFloat(i), 0, scrollView.frame.width, scrollView.frame.height)
-            scrollView.addSubview(imageView)
-        }
-        scrollView.contentSize = CGSizeMake(scrollView.frame.width*5,scrollView.frame.height)
-        scrollView.contentOffset = CGPointMake(scrollView.frame.width, 0)
-        self.edgesForExtendedLayout = UIRectEdge.None
-        self.automaticallyAdjustsScrollViewInsets = false
-        scrollView.delegate = self
-        pageControl.numberOfPages = 3
-        pageControl.currentPageIndicatorTintColor = UIColor.redColor()
-        pageControl.pageIndicatorTintColor = UIColor.whiteColor()
-        pageControl.frame = CGRectMake(260, 130, 40, 10)
-        pageControl.addTarget(self, action: #selector(pageChanged(_:)),forControlEvents: UIControlEvents.ValueChanged)
+                imageView.frame = CGRectMake(scrollView.frame.width*CGFloat(i), 0, scrollView.frame.width, scrollView.frame.height)
+                scrollView.addSubview(imageView)
+            }
+            scrollView.contentSize = CGSizeMake(scrollView.frame.width*5,scrollView.frame.height)
+            scrollView.contentOffset = CGPointMake(scrollView.frame.width, 0)
+            self.edgesForExtendedLayout = UIRectEdge.None
+            self.automaticallyAdjustsScrollViewInsets = false
+            scrollView.delegate = self
+            pageControl.numberOfPages = 3
+            pageControl.currentPageIndicatorTintColor = UIColor.redColor()
+            pageControl.pageIndicatorTintColor = UIColor.whiteColor()
+            pageControl.frame = CGRectMake(260, 100, 40, 10)
+            pageControl.addTarget(self, action: #selector(pageChanged(_:)),forControlEvents: UIControlEvents.ValueChanged)
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
                     self.timer = NSTimer.scheduledTimerWithTimeInterval(self.rollingTime, target: self, selector: #selector(self.next), userInfo: nil, repeats: true)
                     NSRunLoop.currentRunLoop().run()
                 }
-        cell.addSubview(scrollView)
-        cell.addSubview(pageControl)
+            cell.addSubview(scrollView)
+            cell.addSubview(pageControl)
+        }
+        //商品大类窗口
+        else if(indexPath.row == 1){
+            collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+            serviceController.getData(){(array) in
+                self.array = array as! [Goodsclass]
+                dispatch_async(dispatch_get_main_queue(),{self.collectionView.reloadData()})
+            }
+            collectionView.frame = CGRectMake(0, 0, 320, 130)
+            collectionView.showsHorizontalScrollIndicator = false
+            collectionView.backgroundColor = UIColor.whiteColor()
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            cell.addSubview(collectionView)
+        }
         return cell
     }
-
     
     func next() {
        scrollView.setContentOffset(CGPoint(x: (scrollView.frame.width * CGFloat(self.pageControl.currentPage + 2)), y: 0), animated: true)
@@ -132,26 +144,31 @@ class HomeViewController: UITableViewController{
         frame.origin.y = 0
         scrollView.scrollRectToVisible(frame, animated:true)
     }
-
     
-//    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-//    {
-//        return self.array.count
-//    }
-//    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
-//    {
-//        let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
-//        (cell.contentView.viewWithTag(101) as! UIImageView).image = UIImage(named: String(self.array[indexPath.row].id))
-//        (cell.viewWithTag(201) as! UILabel).text = self.array[indexPath.row].gcName
-//        cell.backgroundColor = UIColor.whiteColor()
-//        return cell
-//    }
-//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets{
-//        return UIEdgeInsetsMake(10, 15, 10, 15)
-//    }
-//    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
-//    {
-//        //点击事件
-//    }
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return self.array.count
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        let imageView = UIImageView(image: UIImage(named: "商品大类"+String(self.array[indexPath.row].id)))
+        imageView.frame = CGRectMake(6, 0, 38, 38)
+        let label = UILabel(frame: CGRectMake(0,40,50,10))
+        label.text = self.array[indexPath.row].gcName
+        label.textAlignment = NSTextAlignment.Center
+        label.font = UIFont(name: ".SFUIText-Regular", size: 10)
+        cell.backgroundColor = UIColor.whiteColor()
+        cell.addSubview(imageView)
+        cell.addSubview(label)
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets{
+        return UIEdgeInsetsMake(10, 15, 10, 15)
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    {
+        //点击事件
+    }
 }
 
