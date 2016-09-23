c5a395047c0e7c6843bdb21c43185114bb155eea
黄伟祥 <75646880@qq.com>
2016-09-23 17:56:21 +0800

    等待解决的线程问题

--- evmall/controller/HomeViewController.swift
+++ evmall/controller/HomeViewController.swift
@@ -22,7 +22,7 @@ class HomeViewController: UITableViewController,UICollectionViewDelegate,UIColle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        NSThread.detachNewThreadSelector(#selector(self.loadData), toTarget: self, withObject: nil)
         tableView.separatorStyle = UITableViewCellSeparatorStyle.None
     }
 
@@ -30,6 +30,19 @@ class HomeViewController: UITableViewController,UICollectionViewDelegate,UIColle
         super.didReceiveMemoryWarning()
     }
 
+    func loadData() {
+        getData("advertising"){(array) in
+            self.advertisingArray = array as! [Advertising]
+        }
+        getData("goodsclass"){(array) in
+            self.goodsclassArray = array as! [Goodsclass]
+//            dispatch_async(dispatch_get_main_queue(),{self.goodsclassCollectionView.reloadData()})
+        }
+        getData("goods"){(array) in
+            self.goodsArray = array as! [Goods]
+        }
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
@@ -57,54 +70,47 @@ class HomeViewController: UITableViewController,UICollectionViewDelegate,UIColle
             scrollView.showsVerticalScrollIndicator = false
             scrollView.scrollEnabled = true
             scrollView.frame = CGRectMake(0, 0, 320, 120)
-            getData("advertising"){(array) in
-                self.advertisingArray = array as! [Advertising]
-                for i in 0...self.advertisingArray.count+1 {
-                    let imageUrlString:String
-                    switch i {
-                    case 0:
-                        imageUrlString = self.advertisingArray[self.advertisingArray.count-1].pictureUrl
-                    case self.advertisingArray.count+1:
-                        imageUrlString = self.advertisingArray[0].pictureUrl
-                    default:
-                        imageUrlString = self.advertisingArray[i-1].pictureUrl
-                    }
-                    let url:NSURL! = NSURL(string: imageUrlString)
-                    let data:NSData! = NSData(contentsOfURL: url)
-                    let image = UIImage(data: data)
-                    let imageView =  UIImageView(image: image)
-                    print()
-                    imageView.frame = CGRectMake(self.scrollView.frame.width*CGFloat(i), 0, self.scrollView.frame.width, self.scrollView.frame.height)
-                    self.scrollView.addSubview(imageView)
+            print(self.advertisingArray.count)
+            for i in 0...self.advertisingArray.count+1 {
+                let imageUrlString:String
+                switch i {
+                case 0:
+                    imageUrlString = self.advertisingArray[self.advertisingArray.count-1].pictureUrl
+                case self.advertisingArray.count+1:
+                    imageUrlString = self.advertisingArray[0].pictureUrl
+                default:
+                    imageUrlString = self.advertisingArray[i-1].pictureUrl
                 }
-                    
-                dispatch_async(dispatch_get_main_queue(),{self.pageControl.numberOfPages = self.advertisingArray.count
-                    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.width*CGFloat(self.advertisingArray.count+2),self.scrollView.frame.height)}
-                )
+                let url:NSURL! = NSURL(string: imageUrlString)
+                let data:NSData! = NSData(contentsOfURL: url)
+                let image = UIImage(data: data)
+                let imageView =  UIImageView(image: image)
+                
+                imageView.frame = CGRectMake(self.scrollView.frame.width*CGFloat(i), 0, self.scrollView.frame.width, self.scrollView.frame.height)
+                scrollView.addSubview(imageView)
             }
-            self.scrollView.contentOffset = CGPointMake(self.scrollView.frame.width, 0)
+            pageControl.numberOfPages = self.advertisingArray.count
+            scrollView.contentSize = CGSizeMake(self.scrollView.frame.width*CGFloat(self.advertisingArray.count+2),self.scrollView.frame.height)
+
+            scrollView.contentOffset = CGPointMake(scrollView.frame.width, 0)
             self.edgesForExtendedLayout = UIRectEdge.None
             self.automaticallyAdjustsScrollViewInsets = false
-            self.scrollView.delegate = self
-            self.pageControl.currentPageIndicatorTintColor = UIColor.redColor()
-            self.pageControl.pageIndicatorTintColor = UIColor.whiteColor()
-            self.pageControl.frame = CGRectMake(260, 100, 40, 10)
-            self.pageControl.addTarget(self, action: #selector(self.pageChanged(_:)),forControlEvents: UIControlEvents.ValueChanged)
+            scrollView.delegate = self
+            pageControl.currentPageIndicatorTintColor = UIColor.redColor()
+            pageControl.pageIndicatorTintColor = UIColor.whiteColor()
+            pageControl.frame = CGRectMake(260, 100, 40, 10)
+            pageControl.addTarget(self, action: #selector(pageChanged(_:)),forControlEvents: UIControlEvents.ValueChanged)
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
                     self.timer = NSTimer.scheduledTimerWithTimeInterval(self.rollingTime, target: self, selector: #selector(self.next), userInfo: nil, repeats: true)
                     NSRunLoop.currentRunLoop().run()
                 }
-            self.scrollView.backgroundColor = UIColor.greenColor()
-            cell.addSubview(self.scrollView)
-            cell.addSubview(self.pageControl)
+            cell.addSubview(scrollView)
+            cell.addSubview(pageControl)
         }
         //商品大类窗口
         else if(indexPath.row == 1){
             goodsclassCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "goodsclassCell")
-            getData("goodsclass"){(array) in
-                self.goodsclassArray = array as! [Goodsclass]
-                dispatch_async(dispatch_get_main_queue(),{self.goodsclassCollectionView.reloadData()})
-            }
+            dispatch_async(dispatch_get_main_queue(),{self.goodsclassCollectionView.reloadData()})
             goodsclassCollectionView.frame = CGRectMake(0, 0, 320, 130)
             goodsclassCollectionView.backgroundColor = UIColor.whiteColor()
             goodsclassCollectionView.delegate = self
@@ -113,10 +119,7 @@ class HomeViewController: UITableViewController,UICollectionViewDelegate,UIColle
         }
         else if(indexPath.row == 2){
             goodsCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "goodsCell")
-            getData("goods"){(array) in
-                self.goodsArray = array as! [Goods]
-                dispatch_async(dispatch_get_main_queue(),{self.goodsCollectionView.reloadData()})
-            }
+            dispatch_async(dispatch_get_main_queue(),{self.goodsCollectionView.reloadData()})
             goodsCollectionView.frame = CGRectMake(0, 0, 320, 130)
             goodsCollectionView.backgroundColor = UIColor.whiteColor()
             goodsCollectionView.delegate = self
